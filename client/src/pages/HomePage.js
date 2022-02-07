154ff9313e91b55d9f0e5535f454c3b6702b7583
Jonathan Nguyen <jnganguyen3@gmail.com>
2022-02-07 15:43:45 -0500

    added some styling to the home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import Layout from './components/Layout';
-
 function HomePage() {
   return (
-    <Layout>
-      <div className="HomePage">
-        <p>This is the homepage</p>
+    <div className="HomePage text-white font-bold">
+      <div className="flex items-center justify-center absolute inset-0">
+        <div className="">
+          <body className="text-center">
+            <h1 className="
+            text-6xl 
+            font-bold">
+              Mind-Space
+            </h1>
+            <p>Welcome to Mind-Space. A modern take on Eliza.</p>
+          </body>
+
+          <input />
+        </div>
       </div>
-    </Layout>
+    </div>
   )
 }
 
