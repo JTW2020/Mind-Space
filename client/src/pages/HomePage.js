5d99895741d203618fc08c9c81ce9e4969bf36a6
Jonathan Nguyen <jnganguyen3@gmail.com>
2022-02-08 21:04:18 -0500

    has the dummy frontend

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -14,10 +14,23 @@ function HomePage() {
             <p className="mt-2">Welcome to Mind-Space. A modern take on Eliza.</p>
           </body>
         </div>
-        <div className="mt-6">
-          <label>Your input:</label>
+        <div className="container py-2 px-4 mt-10 w-4/12 text-black bg-zinc-100 h-4/12 rounded rounded-md">
+          <a>Eliza: Is something troubling you?</a>
           <br />
-          <input className="text-black w-full mt-5" />
+          <a>You: I am depressed.</a>
+          <br />
+          <a>Eliza: What do you think is causing this?</a>
+        </div>
+        <div className="flex mt-6 items-center justify-center align-middle">
+          <div className="mr-4">
+            <label className="pr-3">You:</label>
+            <input className="text-black w-full" />
+          </div>
+          <div className="">
+            <button className="bg-purple-500 mt-4 rounded rounded-sm py-1 px-4">
+              Send to Eliza
+            </button>
+          </div>
         </div>
       </div>
     </div>
