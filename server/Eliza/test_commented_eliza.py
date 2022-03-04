77a5fd79fc46d7fbebc83440c06d4b549f43bb6d
Joel Thompson <jt10280@georgiasouthern.edu>
2022-03-04 13:22:05 -0500

    Added new tests and context file changes for  eliza.
    -added new tests to confirm whether eliza switches to disorder context sucessfully.
    -added new test to confirm whether eliza switched contexts to anger correctly.
    -changed keyword 'angry' to 'anger' as the keyword to switch to the anger.txt context.
    -added new test to check if eliza is referencing the anger context correctly.
    -added new test to check if eliza outputs the exit statement once the exit keyword has been given by the user.

--- server/Eliza/test_commented_eliza.py
+++ server/Eliza/test_commented_eliza.py
@@ -7,7 +7,7 @@ def test_ContextSwitchDepressed(): #tests if eliza successfully switched context
     "I am depressed."
     assert el.respond("I see. Please, tell me about how you've been feeling down.") == "I see. Please, tell me about how you've been feeling down."
 
-def test_DepressedContext(): #tests if eliza successfully realizes that it is in the correct context.
+def test_DepressedContext(): #tests if eliza successfully realizes that it is in the depressed context.
     el = commented_eliza.Eliza()
     el.load('depressed.txt')
     'I am depressed'
@@ -19,15 +19,37 @@ def test_ContextSwitchAnxious(): #tests if eliza successfully switched contexts 
     "I am anxious."
     assert el.respond("I see. Tell me about how you've been feeling on edge, please.") == "I see. Tell me about how you've been feeling on edge, please."
 
-def test_AnxiousContext():
+def test_AnxiousContext(): # tests if eliza is sucessfully referencing the anxious context
     el = commented_eliza.Eliza()
     el.load('anxious.txt')
     'I am anxious'
-    assert el.respond("I know that you are anxious. It is going to be okay.") == "I know that you are anxious. It is going to be okay."
+    assert el.respond("I know that you are anxious. Is anything contributing to why you feel (2)?") == "I know that you are anxious. Is anything contributing to why you feel (2)?"
 
-def test_ContextSwitchDisorder(): #tests uf eliza successfully switched contexts from inbetween.txt to disorder.txt
+def test_ContextSwitchDisorder(): #tests if eliza successfully switched contexts from inbetween.txt to disorder.txt via string matching.
     el = commented_eliza.Eliza()
     el.load('inbetween.txt')
-    "I am hungry."
+    "I think I have a Disorder."
     assert el.respond("I see. Please, tell me about why you've been feeling this way.") == "I see. Please, tell me about why you've been feeling this way."
 
+#def test_DisordeContext():     Cannot test disorder context because there is no unique response given when keyword is recieved from user input that triggers the context confirmation
+ #   el = commented_eliza.Eliza()
+  #  el.load('disorder.txt')
+  #  assert el.respond()
+
+def test_ContextSwitchAnger(): #tests if eliza successfully switched contexts from inbetween.txt to anger.txt via string matching.
+    el = commented_eliza.Eliza()
+    el.load('inbetween.txt')
+    "I am angry"
+    assert el.respond("Understood. Please tell me about how you've been having your temper challenged.") == "Understood. Please tell me about how you've been having your temper challenged."
+
+def test_AngerContext(): #confirms if eliza recognizes it is using the anger context via string matching.
+    el = commented_eliza.Eliza()
+    el.load('anger.txt')
+    "I am angry"
+    assert el.respond("we will not talk about your anger") == "we will not talk about your anger"
+
+def test_ElizaExit(): #tests if eliza exits correctly via string matching.
+    el = commented_eliza.Eliza()
+    el.load('inbetween.txt')
+    'goodbye'
+    assert el.respond("Goodbye.  Thank you for talking to me.") == "Goodbye.  Thank you for talking to me."
